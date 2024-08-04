49a7c9749977b4f3c936a0ef2771fc9630a6de68
Iurii62Rus <kharlamov.yury2017@yandex.ru>
2024-08-04 23:37:22 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,14 @@
+my_dict = {'Yuri': 1994, 'Nikita': 1995, 'Steve': 2018}
+print(my_dict)
+print(my_dict['Yuri'])
+my_dict['refrigerator_аtlant'] = 2000
+my_dict['TV_Haier'] = 2022
+print(my_dict)
+del my_dict['Nikita']
+print(my_dict)
+my_set = {'homeless person on the heating main', 'BMW owner'}
+print(my_set)
+my_set = {5, 6}
+print(my_set)
+my_set.discard(5)
+print(my_set)
